9ccc769aecba50b4d55ea1e4aa45d0736b90059e
choijungp <wjddls980912@naver.com>
2023-05-19 17:34:01 +0900

    [DEP-412] Init: 프로젝트 생성

---
+++ three-days-iOS/three-days-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  three-days-iOS
+//
+//  Created by 최정인 on 2023/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ three-days-iOS/three-days-iOS/three_days_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  three_days_iOSApp.swift
+//  three-days-iOS
+//
+//  Created by 최정인 on 2023/05/19.
+//
+
+import SwiftUI
+
+@main
+struct three_days_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
