df8abd897516b4c7967a38b03520e3fa6fcd19ad
ChoiJungin <jichoi8448@raoncorp.com>
2023-07-12 09:48:16 +0900

    [DEP-417] HomeView 레이아웃 적용

--- three-days-iOS/three-days-iOS/Views/Screens/HomeView/HomeView.swift
+++ three-days-iOS/three-days-iOS/Views/Screens/HomeView/HomeView.swift
@@ -12,61 +12,59 @@ struct HomeView: View {
     
     var body: some View {
         NavigationView {
-            /// 날짜 및 마이페이지 이동
-            HStack {
-                Text(DateUtil.getCurrentDate(format: "M월 d일 EEEE"))
-                    .font(.custom("SUIT-Bold", size: 20))
-                
-                Spacer()
-                
-                // TODO: 마이페이지로 이동 (EmptyView -> MyPageView)
-                NavigationLink(destination: EmptyView()) {
-                    Image(systemName: "person.circle")
-                        .foregroundColor(Color("gray500"))
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                }
-            }
-            .offset(x: 0, y: -350)
-            .padding()
-            
-            /// 습관 만들기 및 습관 목록
-            ScrollView {
-                Text("왜 안나와 ??")
-                
-                if vm.habits.isEmpty {
-                    NavigationLink(destination: HabitCreateView()) {
-                        ZStack {
-                            HStack {
-                                ZStack {
-                                    Rectangle()
-                                        .fill(Color("gray200"))
-                                        .frame(width: 55, height: 55)
-                                        .cornerRadius(15)
-                                    
-                                    Image(systemName: "plus")
-                                        .foregroundColor(Color("gray500"))
-                                }
-                                
-                                Text("습관 만들기")
-                            }
-                            
-                            Rectangle()
-                                .fill(Color("gray500"))
-                                .frame(width: 335, height: 140)
-                                .cornerRadius(20)
-                        }
+            VStack {
+                /// 날짜 및 마이페이지 이동
+                HStack {
+                    Text(DateUtil.getCurrentDate(format: "M월 d일 EEEE"))
+                        .font(.custom("SUIT-Bold", size: 20))
+                    
+                    Spacer()
+                    
+                    // TODO: 마이페이지로 이동 (EmptyView -> MyPageView)
+                    NavigationLink(destination: EmptyView()) {
+                        Image("ic_hamburger")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
                     }
-                } else {
-                    Text("Habits will appear !!")
-                    // TODO: viewModel에 있는 habits array를 뿌려서 보여주기
-//                    ForEach(vm.habits) { habit in
-//
-//                    }
+                }
+                .padding()
+                
+                /// 습관 만들기 및 습관 목록
+                ScrollView {
+                    if vm.habits.isEmpty {
+                        NavigationLink(destination: HabitCreateView()) {
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color("gray100"))
+                                    .frame(width: 335, height: 140)
+                                    .cornerRadius(20)
+                                
+                                HStack {
+                                    Image("ic_add_default")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 29)
+                                    
+                                    
+                                    Text("습관 만들기")
+                                        .font(.custom("SUIT-SemiBold", size: 14))
+                                        .foregroundColor(Color("gray600"))
+                                }
+                            }
+                        }
+                    } else {
+                        Text("Habits will appear !!")
+                        // TODO: viewModel에 있는 habits array를 뿌려서 보여주기
+    //                    ForEach(vm.habits) { habit in
+    //
+    //                    }
+                    }
                 }
             }
         }
         .navigationBarHidden(true)
+        .background(Color("bg"))
     }
 }
 
